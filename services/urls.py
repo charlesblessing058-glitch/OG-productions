39f379d768540aedb7b657c09a4cd5a8debbffd9
charles <charlesblessing058@gmail.com>
2026-05-01 20:59:11 +0300

    Restore: Authentication URLs

--- services/urls.py
+++ services/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     # ONLY the home page for now
     path('', views.home, name='home'),
     path('temp-create-admin/', views_temp.temp_create_admin, name='temp_create_admin'),
+    # Authentication
+    path('login/', views.client_login, name='login'),
+    path('register/', views.client_register, name='register'),
+    path('logout/', views.client_logout, name='logout'),
 ]
+
 
