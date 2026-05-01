da196b87b363b23167b676e6c6bb04112ea4eddb
charles <charlesblessing058@gmail.com>
2026-05-01 22:32:14 +0300

    Fix: Replace login URL tags in template

--- services/urls.py
+++ services/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'services'
 
 urlpatterns = [
-    # ONLY the home page for now
     path('', views.home, name='home'),
+    path('login/', views.client_login, name='login'),
+    path('register/', views.client_register, name='register'),
+    path('logout/', views.client_logout, name='logout'),
 ]
